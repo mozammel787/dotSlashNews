376ab34cbd6a5594c16150ff757346589e9e2d20
mozammel787 <mhfaryanemon@gmail.com>
2024-06-05 21:44:12 +0600

    fixed layout

--- src/Components/Global/Header.jsx
+++ src/Components/Global/Header.jsx
@@ -5,12 +5,41 @@ const Header = () => {
   const { logOut, user } = useAuth();
   return (
     <>
-      <div className="navbar bg-base-200 ">
+      <div className="navbar bg-base-200 md:py-5">
         <div className="container mx-auto justify-between">
-          <div className="">
-            <Link to={"/"} className="btn btn-ghost text-xl">
-              <img className="w-[90px]" src="logo.png" alt="" />
+          <div className="flex items-center">
+          <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/all-news"}>All News</Link>
+                </li>
+                <li>
+                  <Link to={"/catagory/business"}>Business</Link>
+                </li>
+                <li>
+                  <Link to={"/catagory/sports"}>Sports</Link>
+                </li>
+                <li>
+                  <Link to={"/catagory/entertainment"}>Entertainment</Link>
+                </li>
+                <li>
+                  <Link to={"/catagory/science"}>Science</Link>
+                </li>
+                <li>
+                  <Link to={"/catagory/technology"}>Technology</Link>
+                </li>
+      </ul>
+    </div>
+            <Link to={"/"} className=" text-xl">
+              <img className="w-[70px] md:w-[90px]" src="logo.png" alt="" />
             </Link>
+            
           </div>
           <div className="navbar-center">
             <div className=" hidden lg:flex">
@@ -40,7 +69,7 @@ const Header = () => {
             </div>
           </div>
           <div className="flex items-center gap-5">
-            <Link to={"search"} className="btn btn-ghost btn-circle">
+            <Link to={"search"} className="">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
--- src/Layout/DashbordLayOut.jsx
+++ src/Layout/DashbordLayOut.jsx
@@ -36,15 +36,15 @@ const DashbordLayOut = () => {
           ></label>
           <div className="flex flex-col justify-between h-full p-3 w-60 bg-gray-50 text-gray-800">
             <div className="space-y-3">
-              <div className="flex items-center justify-between">
+              <Link to={"/"} className="flex  justify-center">
                 <img className="w-20" src={"logo.png"} alt="" />
 
                 
-              </div>
+              </Link>
 
               <div className="flex-1">
-                <ul className="pt-2 pb-4 space-y-3 text-sm ">
-                  <li className="rounded-sm   hover:bg-gray-200">
+                <ul className="pt-10  pb-4 space-y-4 text-sm ">
+                  <li className="hover:shadow border-0 bg-gray-100 hover:bg-gray-200  rounded btn w-full justify-start  ">
                     <Link
                       to={"/"}
                       rel="noopener noreferrer"
@@ -53,14 +53,14 @@ const DashbordLayOut = () => {
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 512 512"
-                        className="w-5 h-5 fill-current text-gray-600"
+                        className="w-5 h-5 fill-current text-gray-600  "
                       >
                         <path d="M469.666,216.45,271.078,33.749a34,34,0,0,0-47.062.98L41.373,217.373,32,226.745V496H208V328h96V496H480V225.958ZM248.038,56.771c.282,0,.108.061-.013.18C247.9,56.832,247.756,56.771,248.038,56.771ZM448,464H336V328a32,32,0,0,0-32-32H208a32,32,0,0,0-32,32V464H64V240L248.038,57.356c.013-.012.014-.023.024-.035L448,240Z"></path>
                       </svg>
                       <span>Home</span>
                     </Link>
                   </li>
-                  <li className="rounded-sm  hover:bg-gray-200">
+                  <li className="hover:shadow border-0 bg-gray-100 hover:bg-gray-200  rounded btn w-full justify-start ">
                     <Link
                       to={"/dashbord"}
                       rel="noopener noreferrer"
@@ -143,7 +143,7 @@ const DashbordLayOut = () => {
                       <span>My Profile</span>
                     </Link>
                   </li>
-                  {/* <li className="rounded-sm  hover:bg-gray-200">
+                  {/* <li className="hover:shadow bg-gray-100 hover:bg-gray-200  rounded btn w-full justify-start ">
                     <Link to={"edit-profile"}
                       rel="noopener noreferrer"
                       className="flex items-center p-2 space-x-3 rounded-md"
@@ -177,7 +177,7 @@ const DashbordLayOut = () => {
                       <span>Edit Profile</span>
                     </Link>
                   </li> */}
-                  <li className="rounded-sm  hover:bg-gray-200">
+                  <li className="hover:shadow border-0 bg-gray-100 hover:bg-gray-200  rounded btn w-full justify-start ">
                     <Link to={`my-post/${user.email}`}
                       rel="noopener noreferrer"
                       className="flex items-center p-2 space-x-3 rounded-md"
@@ -186,7 +186,7 @@ const DashbordLayOut = () => {
                       <span>My Post</span>
                     </Link>
                   </li>
-                  <li className="rounded-sm  hover:bg-gray-200">
+                  <li className="hover:shadow border-0 bg-gray-100 hover:bg-gray-200  rounded btn w-full justify-start ">
                     <Link to={"add-post"}
                       rel="noopener noreferrer"
                       className="flex items-center p-2 space-x-3 rounded-md"
